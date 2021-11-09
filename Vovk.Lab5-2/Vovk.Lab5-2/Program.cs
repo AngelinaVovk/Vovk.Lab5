a61a72227a102b8acb81af354960a800ff8c90e4
Angelina Vovk <vovkav21@oiate.ru>
2021-11-09 14:46:30 +0300

    Добавьте файлы проекта.

---
+++ Vovk.Lab5-2/Vovk.Lab5-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Vovk.Lab5
+{
+    class Program
+    {
+        static double Func(double x, double k)
+        {
+            double F = (x * Math.Sin(x)) / Math.Pow(k, 3);
+            return F;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой ряд");
+            Console.WriteLine("Расчет суммы ряда");
+            double S = 0;
+            double x = Convert.ToDouble(Console.ReadLine());
+            for (int k = 1; k < 7; k++)
+            {
+                double F = Func(x, k);
+                S += F;
+                Console.WriteLine(S);
+            }
+            Console.WriteLine("Расчет произведения ряда");
+            for (int k = 1; k < 7; k++)
+            {
+                double F = Func(x, k);
+                S *= F;
+                Console.WriteLine(S);
+            }
+        }
+    }
+}
+
+
+
+        
+
+
+
+
+
---
+++ Vovk.Lab5-3/Vovk.Lab5-3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace Vovk.Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string str = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите цифру для удаления");
+            string y = Convert.ToString(Console.ReadLine());
+            switch (y)
+            {
+                case "0":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("0", ""));
+                    }
+                    break;
+
+                case "1":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("1", ""));
+                    }
+                    break;
+                case "2":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("2", ""));
+                    }
+                    break;
+                case "3":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("3", ""));
+                    }
+                    break;
+                case "4":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("4", ""));
+                    }
+                    break;
+                case "5":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("5", ""));
+                    }
+                    break;
+                case "6":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("6", ""));
+                    }
+                    break;
+                case "7":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("7", ""));
+                    }
+                    break;
+                case "8":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("8", ""));
+                    }
+                    break;
+                case "9":
+                    {
+                        Console.WriteLine("Результат : " + str.Replace("9", ""));
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
